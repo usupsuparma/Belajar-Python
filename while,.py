5f1739dea2c2c6a41445569688fc067192ff5d9a
usupsuparma <usup.suparma7@gmail.com>
2018-05-06 21:23:30 +0700

    materi tentang while

---
+++ while,.py
@@ -0,0 +1,14 @@
+angka = 0
+while angka < 5:
+    print("angka adalah: ",angka)
+    angka += 1
+print("ini diluar while")
+start = True
+angka = 0
+while start:
+    print("mencari bilangan, ",angka)
+    if angka is 10:
+        start = False
+        print("angaka ditemukan")
+    angka += 1
+print("keluar dari loop")
